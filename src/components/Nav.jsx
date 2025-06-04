d836e699141fefa4316deb102075fc096c2eb71a
kottor <gardien243@gmail.com>
2025-06-04 14:18:10 +0200

    mise a jour sass

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,7 +25,7 @@ export default function NavBar({modif_filter,filter,categorie}) {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <NavLink className="nav-link active" aria-current="page" to={'/'}>
+                            <NavLink className="nav-link" aria-current="page" to={'/'}>
                                 Home
                             </NavLink>
                         </li>
